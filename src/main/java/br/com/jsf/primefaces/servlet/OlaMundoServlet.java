ba3469ff6afd6b7145a5c19d877dbff7eca35e61
Karina Mayumi Hirata <karinamahi@hotmail.com>
2015-12-27 16:52:25 -0200

    Added project settings

---
+++ src/main/java/br/com/jsf/primefaces/servlet/OlaMundoServlet.java
@@ -0,0 +1,43 @@
+package br.com.jsf.primefaces.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class OlaMundoServlet
+ */
+@WebServlet("/ola-mundo")
+public class OlaMundoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public OlaMundoServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.print("<html>");
+		out.print("<body><h1>Olá mundo</h1></body>");
+		out.print("</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
